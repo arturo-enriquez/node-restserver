8f1cab0982b7df50c84834211d875b0ed40350b3
Arturo Enriquez <arturoccompany@gmail.com>
2019-01-23 03:29:10 -0700

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlDB;
 if ( process.env.NODE_ENV == 'dev' ) {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:abc123@ds253879.mlab.com:53879/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
