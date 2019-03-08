0c983b6694997b7d4427239c8be2559241a673f7
Arturo Enriquez <arturoccompany@gmail.com>
2019-03-08 16:21:00 -0700

    Login normal

--- server/config/config.js
+++ server/config/config.js
@@ -9,6 +9,16 @@ process.env.PORT = process.env.PORT ||  3000;
 // ===========================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+// ===========================
+//    Vencimiento del Token
+// ===========================
+// 60 segunso, 60 minutos, 24 horas, 30 dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// ===========================
+//    SEED de autenticacion
+// ===========================
+process.env.SEED = process.env.SEED  || 'seed-de-desarrollo';
 
 // ===========================
 //    Base de datos
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
+
 const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
 
 
 const app = express();
 
-app.get('/usuario', function (req, res) {
-    // { estado: true }
+app.get('/usuario', verificaToken, (req, res) => {
+
     let estado = req.query.estado || true;
     estado = Boolean(estado);
 
@@ -40,7 +42,7 @@ app.get('/usuario', function (req, res) {
 
 }); 
 
-app.post('/usuario', function (req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], (req, res) => { 
 
     let body = req.body;
 
@@ -67,7 +69,7 @@ app.post('/usuario', function (req, res) {
 
 });
 
-app.put('/usuario/:id', function (req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre','email','img','role','estado']);
@@ -86,7 +88,7 @@ app.put('/usuario/:id', function (req, res) {
     })
 });
 
-app.delete('/usuario/:id', function (req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
     
     let id = req.params.id;
 
